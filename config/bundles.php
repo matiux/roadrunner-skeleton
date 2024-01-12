ed17c18e51340f0623eb64f47a28a79c3e205bef
Matteo Galacci <m.galacci@gmail.com>
2024-01-13 00:49:58 +0100

    adds symfony files

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+];
